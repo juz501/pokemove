e84f5cfe5d9458453fe5d424d100fb97d56a3492
juz <julianc@gmail.com>
2017-05-16 01:08:43 +1000

    updates

--- src/pokemove/pokemove.go
+++ src/pokemove/pokemove.go
@@ -6,10 +6,12 @@ import (
   "fmt"
   "log"
   "net/http"
+  "io/ioutil"
+  "strings"
+  "strconv"
   "os"
 
   "github.com/chromatixau/negroni"
-  "github.com/chromatixau/gomiddleware"
 )
 
 func main() {
@@ -21,16 +23,13 @@ func main() {
   defer errorLog.Close()
 
   n := negroni.New()
-  l := gomiddleware.NewLoggerWithStream( errorLog )
-  r := negroni.NewRecovery()
-  r.Logger = l
-  r.PrintStack = false
+  l := negroni.NewLoggerWithStream( errorLog )
   m := http.NewServeMux()
 
   handleRoutes( m )
 
-  n.Use(r)
-  n.Use(l)
+  n.Use( l )
+
   n.UseHandler( m )
 
   log.Fatal( http.ListenAndServe( ":18885", n ) )
@@ -46,15 +45,23 @@ func handleRoutes( m *http.ServeMux ) {
         text = r.URL.Query().Get( "text" )
       }
       url := "https://hooks.slack.com/services/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"
-      channel := r.URL.Query().Get( "channel_id" )
+      user_name := r.URL.Query().Get( "username" )
+      if user_name == "" {
+        user_name = "julian"
+      }
       if text == "" {
         fmt.Fprintf( w, "%s", about() )
-        w.WriteHeader( http.StatusOK )
       } else {
-        go sendMoveInfo( &url, &channel, &text )
-        w.WriteHeader( http.StatusOK )
+        go sendMoveInfo( url, user_name, text, w )
+        fmt.Fprintf( w, "loading move data :loading:" )
       }
+    default:
+      w.WriteHeader( http.StatusOK )
     }
+  })
+
+  m.HandleFunc( "/favicon.ico", func( w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf( w, "%s", about() )
   })
 }
 
@@ -66,11 +73,14 @@ func about() ( s string ) {
 type Message struct {
   Text string `json:"text"`
   Channel string `json:"channel"`
+  Username string `json:"username"`
+  Markdown bool `json:"mrkdwn"`
 }
 
-func sendMoveInfo( url *string, channel *string, move_name *string ) {
-  s := fmt.Sprintf( "%s", *move_name )
-  m := Message{ s, *channel }
+func sendMoveInfo( url string, user_name string, move_name string, w http.ResponseWriter ) {
+  s := getMoveResult( move_name )
+
+  m := Message{ s, "@" + user_name, "pokemove", true }
   message, err := json.Marshal( m )
   if err != nil {
     panic( "json marshall failed" )
@@ -78,17 +88,78 @@ func sendMoveInfo( url *string, channel *string, move_name *string ) {
   _ = sendToSlack( url, message )
 }
 
-func sendToSlack( url *string, message []byte) (resp *http.Response) {
+func sendToSlack( url string, message []byte) (resp *http.Response) {
   client := http.Client{}
   body := bytes.NewBuffer( message )
-  req, err := http.NewRequest( "POST", *url, body )
+  req, err := http.NewRequest( "POST", url, body )
   if err != nil {
-    panic( "error in request" )
+    return
   }
   req.Header.Set( "Content-Type", "application/json" )
-  resp, err2 := client.Do( req )
-  if err2 != nil {
-    panic( "error in request 2" )
+  resp, _ = client.Do( req )
+  defer resp.Body.Close()
+  return
+}
+
+type Data struct {
+  Accuracy int64 `json:"accuracy"`
+  EffectEntries []Effect `json:"effect_entries"`
+  EffectChance int64 `json:"effect_chance"`
+  Name string `json:"name"`
+  PP int64 `json:"pp"`
+  Power int64 `json:"power"`
+  Target Target `json:"target"`
+  Type Type `json:"type"`
+}
+
+type Effect struct {
+  Language Language `json:"language"`
+  Effect string `json:"effect"`
+  ShortEffect string `json:"short_effect"`
+}
+
+type Language struct {
+  Name string `json:"name"`
+}
+
+type Target struct {
+  Name string `json:"name"`
+}
+
+type Type struct {
+  Name string `json:"name"`
+}
+
+func getMoveResult( move_name string ) ( move_desc string ) {
+  res, err := http.Get( "http://pokeapi.co/api/v2/move/" + move_name )
+  if err != nil {
+    move_desc = ""
+    return
   }
+  move_data, err := ioutil.ReadAll( res.Body )
+  var m Data
+  _ = json.Unmarshal( move_data, &m )
+  if err != nil {
+    move_desc = ""
+    return
+  }
+
+  if m.Name != "" {
+    move_desc = "*" + strings.Replace( strings.ToUpper( m.Name ), "-", " ", -1 ) + ":* \n"
+    move_desc += "• Accuracy : " + strconv.FormatInt( m.Accuracy, 10 ) + "\n"
+    move_desc += "• PP : " + strconv.FormatInt( m.PP, 10 ) + "\n"
+    move_desc += "• Power : " + strconv.FormatInt( m.Power, 10 ) + "\n"
+    move_desc += "• Target : " + m.Target.Name + "\n"
+    move_desc += "• Type : " + m.Type.Name + "\n"
+    effect_chance := strconv.FormatInt( m.EffectChance, 10 )
+    move_desc += "• Effect Chance : " + effect_chance + "\n"
+
+    for _, entry := range m.EffectEntries {
+      move_desc += "• Language : " + entry.Language.Name + "\n"
+      move_desc += "• Effect short : " + strings.Replace( entry.ShortEffect, "$effect_chance", effect_chance, -1 ) + "\n"
+      move_desc += "• Effect : \n>>>" + strings.Replace( entry.Effect, "$effect_chance", effect_chance, -1 ) + "\n"
+    }
+  }
+
   return
 }
